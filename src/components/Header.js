8f966ff1b45a1cd9a641c24b7906e8bc96226b71
Mohamed Beshri Amer <mohamed.beshri.amer@gmail.com>
2023-02-14 23:10:09 +0200

    adding first stage

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import Nav from "./Nav";
+export default function Header(){
+    return (<header>
+        <img src="" alt="logo" />
+        <Nav></Nav>
+        
+    </header>)
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,9 @@
+export default function Nav(){
+    return(<nav>
+            <ul>
+                <li><a href="#" >Home</a></li>
+                <li><a href="#" >About</a></li>
+                <li><a href="#">Contact </a></li>
+            </ul>
+        </nav>)
+}
